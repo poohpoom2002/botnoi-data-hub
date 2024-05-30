b4c6dafa4da1cc976b45f040e132b54ca02515ce
P-Peerapas <37375504+P-PrPas@users.noreply.github.com>
2024-05-30 17:38:18 +0700

    Update lineChart.py
    Add three graphs: Daily income for this month, Today's Income, and Yesterday's Income
    Change the name of the select box: Day --> Daily income for this year

--- lineChart.py
+++ lineChart.py
@@ -97,7 +97,7 @@ def plot_revenue_trend(df, period):
         fig.update_traces(textposition='top center')
         fig.update_traces(texttemplate='%{text:,.2f}', textposition='top center', 
                         hovertemplate='Week: %{x}<br>Total Revenue: %{y:,.2f}<extra></extra>')
-    elif period == 'Day':
+    elif period == 'Daily income for this year':
         df['date_label'] = df['datetime'].dt.strftime('%Y-%m-%d')
         daily_revenue = df.groupby('date_label')['price'].sum().reset_index()
         
@@ -107,6 +107,44 @@ def plot_revenue_trend(df, period):
         fig.update_xaxes(tickangle=45, tickformat="%Y-%m-%d", dtick='M1')
         fig.update_traces(texttemplate='%{text:,.2f}', textposition='top center', 
                         hovertemplate='Date: %{x}<br>Total Revenue: %{y:,.2f}<extra></extra>')
+        
+    elif period == 'Daily income for this month' :
+        current_datetime = pd.Timestamp.now()
+        this_month = int(current_datetime.strftime("%m"))
+        df['datetime'] = pd.to_datetime(df['datetime'])
+        df_month = df[df['datetime'].dt.month == this_month]
+        df_month['day'] = df_month['datetime'].dt.day
+        daily_income = df_month.groupby('day')['price'].sum().reset_index()
+        fig = px.line(daily_income, x='day', y='price', markers=True, title='Daily Income Over Time', labels={'day': 'Date', 'price': 'Income'})
+        fig.update_traces(text=daily_income['price'], textposition='top center', line_color = "#1474cd")
+        fig.update_layout(plot_bgcolor='white',paper_bgcolor='white', xaxis=dict(showgrid=True, gridcolor='rgba(200, 200, 200, 0.5)', title=dict(font=dict(color='black'))), 
+                          yaxis=dict(showgrid=True,gridcolor='rgba(200, 200, 200, 0.5)', margin=dict(l=40, r=40, t=40, b=40))
+                          
+    elif period == 'Today Income' :
+        current_datetime = pd.Timestamp.now()
+        daily_date = current_datetime.strftime("%Y-%m-%d")
+        current_day = pd.to_datetime(daily_date)
+        df['datetime'] = pd.to_datetime(df['datetime'])
+        df_today = df[df['datetime'].dt.date == current_day.date() - pd.Timedelta(days=1)]
+        df_today['hour'] = df_today['datetime'].dt.hour
+        hourly_income = df_today.groupby('hour')['price'].sum().reset_index()
+        fig = px.line(hourly_income, x='hour', y='price', markers=True, title=f'Hourly Income for {(current_day - pd.Timedelta(days=1)).date()}', labels={'hour': 'Hour of the Day', 'price': 'Income'})
+        fig.update_traces(text=daily_income['price'], textposition='top center', line_color = "#1474cd")
+        fig.update_layout(plot_bgcolor='white',paper_bgcolor='white',xaxis=dict(showgrid=True, gridcolor='rgba(200, 200, 200, 0.5)', title=dict(font=dict(color='black'))),
+                          yaxis=dict(showgrid=True, gridcolor='rgba(200, 200, 200, 0.5)', title=dict(font=dict(color='black'))),margin=dict(l=40, r=40, t=40, b=40))
+        
+    elif period == 'Yesterday Income' :
+        current_datetime = pd.Timestamp.now()
+        daily_date = current_datetime.strftime("%Y-%m-%d")
+        current_day = pd.to_datetime(daily_date)
+        df['datetime'] = pd.to_datetime(df['datetime'])
+        df_today = df[df['datetime'].dt.date == current_day.date()]
+        df_today['hour'] = df_today['datetime'].dt.hour
+        hourly_income = df_today.groupby('hour')['price'].sum().reset_index()
+        fig = px.line(hourly_income, x='hour', y='price', markers=True, title=f'Hourly Income for {(current_day).date()}', labels={'hour': 'Hour of the Day', 'price': 'Income'})
+        fig.update_traces(text=daily_income['price'], textposition='top center', line_color = "#1474cd")
+        fig.update_layout(plot_bgcolor='white',paper_bgcolor='white',xaxis=dict(showgrid=True, gridcolor='rgba(200, 200, 200, 0.5)', title=dict(font=dict(color='black'))),
+                          yaxis=dict(showgrid=True, gridcolor='rgba(200, 200, 200, 0.5)', title=dict(font=dict(color='black'))),margin=dict(l=40, r=40, t=40, b=40))
     return fig
 #---------------- end here ----------------
 connectOpenAI()
@@ -115,7 +153,7 @@ df_pay  = connectMongo()
 #---------------- start here ----------------
 #ฝากจัด template 
 time_selectbox = st.selectbox( "select time dimension" ,
-    ("Quarter","Month", "Week","Day")
+    ("Quarter","Month", "Week","Daily income for this year","Daily income for this month","Today Income", "Yesterday Income")
 )
 
 if  time_selectbox == "Quarter":
@@ -127,7 +165,18 @@ elif time_selectbox == "Month":
 elif time_selectbox == "Week":
     lineChart_fig = plot_revenue_trend(df_pay ,"Week")
     st.plotly_chart(lineChart_fig)
-elif time_selectbox == "Day":
-    lineChart_fig = plot_revenue_trend(df_pay ,"Day")
+elif time_selectbox == "Daily income for this year":
+    lineChart_fig = plot_revenue_trend(df_pay ,"Daily income for this year")
     st.plotly_chart(lineChart_fig)
+elif time_selectbox == "Daily income for this month":
+    lineChart_fig = plot_revenue_trend(df_pay ,"Daily income for this month")
+    st.plotly_chart(lineChart_fig)
+elif time_selectbox == "Today Income":
+    lineChart_fig = plot_revenue_trend(df_pay ,"Today Income")
+    st.plotly_chart(lineChart_fig)
+elif time_selectbox == "Yesterday Income":
+    lineChart_fig = plot_revenue_trend(df_pay ,"Yesterday Income")
+    st.plotly_chart(lineChart_fig)
+
+
 #---------------- end here ----------------
