160c5c25f4efe44188b9464a7c2dd801b46b26b0
Phuriphut Phokasawat <phuriphut2002@yahoo.com>
2024-05-30 17:07:30 +0700

    combine line graph, prompt, and about data together

--- Dashboard_and_promt.py
+++ Dashboard_and_promt.py
@@ -7,6 +7,7 @@ import pandas as pd
 from pandasai import SmartDataframe
 import cv2
 import hashlib
+import plotly.express as px
 
 @st.cache_resource
 def connectMongo() :
@@ -14,19 +15,39 @@ def connectMongo() :
     # Create a new client and connect to the server
     client = MongoClient(uri, server_api=ServerApi('1'))
     # Send a ping to confirm a successful connection
-    
     try:
         client.admin.command('ping')
         print("Pinged your deployment. You successfully connected to MongoDB!")
         paymentdb = client['prod-tts-payment']
-        df = pd.DataFrame(paymentdb.payment.find())
-        df_str = df.astype(str)
-        sdf = SmartDataframe(df_str)
-        return sdf
+
+        df_pay = pd.DataFrame(paymentdb.payment.find())
+        df_pay_droped = df_pay.drop(columns=['qrcode' ,'transactionid', 'actual_time'	,'sale_code_name'	, 'package_sub','ref1',	'action'	,'subscription',	'sub_id'])
+
+        df_pay_droped = df_pay_droped.astype(str) 
+
+        df_pay_droped['datetime'] = pd.to_datetime(df_pay_droped['datetime'])
+        df_pay_droped['price'] = df_pay_droped['price'].astype(float)
+        df_pay_droped['point'] = df_pay_droped['point'].astype(float)
+
+        df_pay_droped = df_pay_droped[(df_pay_droped.datetime > '2024-01-01 00:00:00') & (df_pay_droped.status == 'True')]
+
+        df_pay_droped['Year'] = df_pay_droped['datetime'].dt.year
+        df_pay_droped['Month'] = df_pay_droped['datetime'].dt.month
+        df_pay_droped['Week'] = df_pay_droped['datetime'].dt.isocalendar().week
+        df_pay_droped['Day'] =df_pay_droped['datetime'].dt.day
+        df_pay_droped['Hour'] = df_pay_droped['datetime'].dt.hour
+        df_pay_droped['Day_of_Week'] =df_pay_droped['datetime'].dt.day_name()
+
+        df_pay_droped['Year'] = df_pay_droped['Year'].astype('Int32')
+        df_pay_droped['Month'] = df_pay_droped['Month'].astype('Int32')
+        df_pay_droped['Week'] = df_pay_droped['Week'].astype('Int32')
+        df_pay_droped['Day'] = df_pay_droped['Day'].astype('Int32')
+        df_pay_droped['Hour'] = df_pay_droped['Hour'].astype('Int32')
+
+        return df_pay_droped.astype(str)
+    
     except Exception as e:
         print(e)
-
-    paymentdb = client['prod-tts-payment']
 
 def connectOpenAI() :
     os.environ["PANDASAI_API_KEY"] = "$2a$10$vdsfzU0rvW1vs8v1G/aMjebe.k5HuuOi3tftrf0E7c.XWH9wknn4a"
@@ -35,9 +56,66 @@ def calculate_file_hash(file_path):
     with open(file_path, "rb") as f:
         file_hash = hashlib.sha256(f.read()).hexdigest()
     return file_hash
+
+def plot_revenue_trend(df, period):
+    df['datetime'] = pd.to_datetime(df['datetime'])
+    df['price'] = df['price'].astype(float)
+
+    if period == 'Quarter':
+        df['quarter'] = df['datetime'].dt.to_period('Q')
+        df['quarter_label'] = df['quarter'].apply(lambda x: f"Quarter {x.quarter} {x.year}")
+
+        quarterly_revenue = df.groupby('quarter')['price'].sum().reset_index()
+        quarterly_revenue = quarterly_revenue.merge(df[['quarter', 'quarter_label']].drop_duplicates(), on='quarter', how='left')
+
+    
+        fig = px.line(quarterly_revenue, x='quarter_label', y='price', title='Quarterly Revenue Trend',
+                  labels={'quarter_label': 'Quarter', 'price': 'Total Revenue'},
+                  markers=True, text='price')
+    
+        fig.update_layout(xaxis=dict(tickformat="%Y-Q%q"))
+        fig.update_traces(texttemplate='%{text:,.2f}', textposition='top center', 
+                      hovertemplate='Quarter: %{x}<br>Total Revenue: %{y:,.2f}<extra></extra>')
+    elif period == 'Month':
+        df['month_name'] = df['datetime'].dt.strftime('%b %Y')
+        df['month_num'] = df['datetime'].dt.to_period('M')
+    
+        monthly_revenue = df.groupby(['month_num', 'month_name'])['price'].sum().reset_index()
+        monthly_revenue = monthly_revenue.sort_values('month_num')
+    
+        fig = px.line(monthly_revenue, x='month_name', y='price', title='Monthly Revenue Trend',
+                  labels={'month_name': 'Month', 'price': 'Total Revenue'},
+                  markers=True, text='price')
+    
+        fig.update_layout(xaxis=dict(tickformat="%b %Y"))
+        fig.update_traces(texttemplate='%{text:,.2f}', textposition='top center', 
+                      hovertemplate='Month: %{x}<br>Total Revenue: %{y:,.2f}<extra></extra>')
+    elif period == 'Week':
+        df['week'] = df['datetime'].dt.to_period('W').astype(str)
+        weekly_revenue = df.groupby('week')['price'].sum().reset_index()
+        fig = px.line(weekly_revenue, x='week', y='price', title='Weekly Revenue Trend',
+                  labels={'week': 'Week', 'price': 'Total Revenue'},
+                  markers=True, text='price')
+    
+        fig.update_layout(xaxis=dict(tickformat="%Y-Q%q"))
+        fig.update_traces(textposition='top center')
+        fig.update_traces(texttemplate='%{text:,.2f}', textposition='top center', 
+                        hovertemplate='Week: %{x}<br>Total Revenue: %{y:,.2f}<extra></extra>')
+    elif period == 'Day':
+        df['date_label'] = df['datetime'].dt.strftime('%Y-%m-%d')
+        daily_revenue = df.groupby('date_label')['price'].sum().reset_index()
+        
+        fig = px.line(daily_revenue, x='date_label', y='price', title='Daily Revenue Trend',
+                    labels={'date_label': 'Date', 'price': 'Total Revenue'},
+                    markers=True)
+        fig.update_xaxes(tickangle=45, tickformat="%Y-%m-%d", dtick='M1')
+        fig.update_traces(texttemplate='%{text:,.2f}', textposition='top center', 
+                        hovertemplate='Date: %{x}<br>Total Revenue: %{y:,.2f}<extra></extra>')
+    return fig
   
 connectOpenAI()
-sdf = connectMongo()
+df_pay  = connectMongo()
+sdf = SmartDataframe(df_pay)
 
 sidebar_style = """
     <style>
@@ -82,14 +160,14 @@ sidebar_style = """
 
 st.markdown(sidebar_style, unsafe_allow_html=True)
 st.sidebar.markdown("<h1 class='sidebar_title'>Botnoi <br> Data Hub</h1>", unsafe_allow_html=True)
-st.sidebar.markdown("<h5 class='lower_title'>Website to help you analyze data from Botnoi Voice with ease and convenience. We have successfully connected to the database!</h5>", unsafe_allow_html=True)
+st.sidebar.markdown("<h5 class='lower_title'>Website to help you analyze data from Botnoi Voice with ease and convenience!</h5>", unsafe_allow_html=True)
 
-selected = st.sidebar.selectbox(
-    "Choose an option",
-    ["Dashboard and prompt", "About Dataset"],
-    key="sidebar_option",
-    format_func=lambda x: x.replace("_", " ").title(),
-)
+# selected = st.sidebar.selectbox(
+#     "Choose an option",
+#     ["Dashboard and prompt", "About Dataset"],
+#     key="sidebar_option",
+#     format_func=lambda x: x.replace("_", " ").title(),
+# )
 
 selectbox_style = """
     <style>
@@ -133,28 +211,24 @@ page_style = """
     </style>
 """
 
-st.markdown(page_style, unsafe_allow_html=True)
-st.markdown("<h1 class='Top_title'>Botnoi Voice Dashboard</h1>", unsafe_allow_html=True)
-st.write("This dashboard showcases key data on Botnoi Voice, including revenue trends and voice download statistics, providing a clear view of financial performance and user engagement.")
+st.markdown("<h1 class='Top_title'>Data Visualization</h1>", unsafe_allow_html=True)
+st.write("Transforms your data into interactive visualizations, allowing you to select time ranges and create a variety of graph types for detailed analysis and presentation.")
+time_selectbox = st.selectbox( "select time dimension" ,
+    ("Quarter","Month", "Week","Day")
+)
 
-
-# dashboard
-
-
-
-
-st.subheader("Our most recent top 10 highest paying customers")
-
-
-# 10 highest paying customers
-
-st.subheader("Dashboard showing customer behavior")
-
-
-
-# customer behavior
-
-
+if  time_selectbox == "Quarter":
+    lineChart_fig = plot_revenue_trend(df_pay ,"Quarter")
+    st.plotly_chart(lineChart_fig)
+elif time_selectbox == "Month":
+    lineChart_fig = plot_revenue_trend(df_pay ,"Month")
+    st.plotly_chart(lineChart_fig)
+elif time_selectbox == "Week":
+    lineChart_fig = plot_revenue_trend(df_pay ,"Week")
+    st.plotly_chart(lineChart_fig)
+elif time_selectbox == "Day":
+    lineChart_fig = plot_revenue_trend(df_pay ,"Day")
+    st.plotly_chart(lineChart_fig)
 
 st.markdown("<h1 class='Top_title'>Let’s explore Insights with PandasAI</h1>", unsafe_allow_html=True)
 st.write("PandasAI is an advanced tool that integrates artificial intelligence capabilities with the Pandas library, enabling users to analyze data more efficiently. Users can prompt in natural language to analyze data directly !")
--- pages/About dataset.py
+++ pages/About dataset.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
+import pandas as pd
 
 sidebar_style = """
     <style>
@@ -48,15 +49,15 @@ st.markdown(sidebar_style, unsafe_allow_html=True)
 st.sidebar.markdown("<h1 class='sidebar_title'>Botnoi <br> Data Hub</h1>", unsafe_allow_html=True)
 
 # Custom text in the sidebar
-st.sidebar.markdown("<h5 class='lower_title'>Website to help you analyze data from Botnoi Voice with ease and convenience. We have successfully connected to the database!</h5>", unsafe_allow_html=True)
+st.sidebar.markdown("<h5 class='lower_title'>Website to help you analyze data from Botnoi Voice with ease and convenience!</h5>", unsafe_allow_html=True)
 
 # Custom option menu in the sidebar
-selected = st.sidebar.selectbox(
-    "Choose an option",
-    ["Dashboard and prompt", "About Dataset"],
-    key="sidebar_option",
-    format_func=lambda x: x.replace("_", " ").title(),
-)
+# selected = st.sidebar.selectbox(
+#     "Choose an option",
+#     ["Dashboard and prompt", "About Dataset"],
+#     key="sidebar_option",
+#     format_func=lambda x: x.replace("_", " ").title(),
+# )
 
 selectbox_style = """
     <style>
@@ -100,15 +101,60 @@ page_style = """
     </style>
 """
 
-st.markdown(page_style, unsafe_allow_html=True)
-st.markdown("<h1 class='Top_title'>Get to know about Dataset </h1>", unsafe_allow_html=True)
-st.write("The data used in the program comes exclusively from the prod-tts-payment database. You can read the variables within the table and find their definitions below.")
+st.title("Get to know about Dataset ")
+st.write("The data used in the program comes exclusively from the prod-tts-payment database. You can read the variables within the table and find their definitions below")
 
+# payment
+st.markdown("""
+    <style>
+    .big-font {
+        font-size:25px !important;
+        font-weight: bold;
+    }
+    </style>
+    <p class="big-font">Payment data</p>
+    """, unsafe_allow_html=True)
+paymentData = {
+    'feature name': ['_id','user_id','package_id','price','datetime','status','point','promotion'],
+    'Description': ['Unique identifier from MongoDB','Unique identifier for the user','Identifier for the purchased packag','Amount paid','Date and time of the transaction','Status of the transaction','Points earned from the transaction','Promotion code used']
+}
+df = pd.DataFrame(paymentData)
+st.write("The payment Data table is designed to store and describe essential details related to transactions. Each entry in the table includes a unique identifier, user information, package details, transaction specifics, and promotional data. This structure helps in tracking and analyzing payment activities and user interactions with different packages and promotions.")
+st.dataframe(df)
 
-st.markdown("<h5 style='font-weight: bold;'>Payment</h5>",unsafe_allow_html=True)
+# Personal form
+st.markdown("""
+    <style>
+    .big-font {
+        font-size:25px !important;
+        font-weight: bold;
+    }
+    </style>
+    <p class="big-font">Personal form data</p>
+    """, unsafe_allow_html=True)
 
+personalData = {
+    'feature name': ['_id','user_id','fullname','age','gender','province','career','use_for','get_by'],
+    'Description': ['Unique identifier for each record in the database','Unique identifier for the user','Full name of the user', 'Age of the user','Gender of the user','Province where the user resides','Career of the user','Purpose for which the service/product is used','Introduction Channel']
+}
+df = pd.DataFrame(personalData)
+st.write("The personal Data table is intended to store user information collected from a form. Each entry includes unique identifiers, personal details such as full name, age, gender, and location, as well as career information and how the user learned about the product or service. This data helps in understanding the user demographics and their interaction with the product.")
+st.dataframe(df)
 
-st.markdown("<h5 style='font-weight: bold;'>Personal form</h5>",unsafe_allow_html=True)
-
-
-st.markdown("<h5 style='font-weight: bold;'>Message</h5>",unsafe_allow_html=True)
+# Message
+st.markdown("""
+    <style>
+    .big-font {
+        font-size:25px !important;
+        font-weight: bold;
+    }
+    </style>
+    <p class="big-font">Message data</p>
+    """, unsafe_allow_html=True)
+messageData = {
+    'feature name': ['_id','user_id','message','datetime','channel','count','provider','language'],
+    'Description': ['Unique identifier from MongoDB', 'Unique identifier for the user','Message content',' Date and time of the transaction','Communication channel',' Number of occurrences','Service provider','Language of the message']
+}
+df = pd.DataFrame(messageData)
+st.write("The message Data table is used to track the messages and voice selections made by users. Each entry contains a unique identifier, user information, message content, timestamp, communication channel, number of occurrences, service provider, and the language of the message. This data helps in analyzing user preferences and interactions with the messaging system.")
+st.dataframe(df)
